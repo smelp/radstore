37b3dae91954e8ff9c62086afdfb9575210f6222
Janne Laukkanen <janne.laukkanen83@gmail.com>
2012-06-10 15:45:08 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ UserTutorial::Application.routes.draw do
 
   #get "users/new"
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
-  #root to: 'static_pages#home'
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
