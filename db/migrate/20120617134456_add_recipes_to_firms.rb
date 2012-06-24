e3b772b8a149085d57ab66743948c86054581806
Janne Laukkanen <janne.laukkanen83@gmail.com>
2012-06-24 16:47:28 +0300

    delete obsolete files

--- db/migrate/20120617134456_add_recipes_to_firms.rb
+++
@@ -1,5 +0,0 @@
-class AddRecipesToFirms < ActiveRecord::Migration
-  def change
-    add_column :recipes, :firm_id, :integer
-  end
-end
--- db/migrate/20120617191313_add_firm_to_materials.rb
+++
@@ -1,5 +0,0 @@
-class AddFirmToMaterials < ActiveRecord::Migration
-  def change
-    add_column :materials, :firm_id, :integer
-  end
-end
