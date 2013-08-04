5ecd22732747bcc6419bfc17b8a8b8aba7e267dd
rmjheino <heinonen.riku@gmail.com>
2013-08-04 21:19:56 +0300

    changed event_type string => integer, because heroku pq

--- db/migrate/20130703121545_create_events.rb
+++ db/migrate/20130703121545_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :target_id
-      t.string :event_type
+      t.integer :event_type
       t.datetime :user_timestamp
       t.string :signature, :limit => 10
       t.string :info
