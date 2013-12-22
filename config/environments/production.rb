b668306cf66dcc25fbe49197c36691710d3e3e44
rmjheino <heinonen.riku@gmail.com>
2013-12-22 17:06:50 +0200

    finally got the hang of rails timezone config...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 UserTutorial::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
+
+  # C is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
